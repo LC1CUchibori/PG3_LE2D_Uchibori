eba00373f0b927568f88dc7f383cdc9dd38866f4
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2024-10-15 11:01:47 +0900

    確認課題01-04

--- main.cpp
+++ main.cpp
@@ -2,37 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-// 奇数か偶数かを判定するためのコールバック関数
-void checkOddEven(int diceRoll, int (*callback)(int), int guess) {
-    int result = callback(diceRoll);
-
-    // 正解か不正解かを判定
-    if ((result == 1 && guess == 1) || (result == 0 && guess == 2)) {
-        printf("正解！\n");
-    }
-    else {
-        printf("不正解！\n");
-    }
-}
-
-// 奇数か偶数かを判定する関数 (奇数なら1、偶数なら0を返す)
-int isOdd(int number) {
-    return number % 2;
+void wait(int seconds) {
+    int ms = 1000 * seconds;
+    clock_t start = clock();
+    while (clock() < start + ms);
 }
 
 int main() {
-    srand(time(NULL)); 
-    int diceRoll = rand() % 6 + 1; 
+    srand(time(0));  // ランダム数生成のシード設定
+    int dice = rand() % 6 + 1;  // 1～6までのサイコロの結果を生成
+    int evenFlag = dice % 2 == 0;  // 偶数かどうかをチェック
 
-    int guess; 
-    printf("サイコロの出目が奇数か偶数かを予想してください (奇数...1 / 偶数...2): ");
-    scanf_s("%d", &guess);
+    printf("サイコロを振りました。半か丁か当ててください (1: 半, 0: 丁): ");
 
-    // 出目をコールバック関数で判定
-    checkOddEven(diceRoll, isOdd, guess);
+    // ユーザーの入力をラムダ式でキャプチャ
+    int (*userInput)() = []() {
+        int response;
+        scanf_s("%d", &response);
+        return response;
+        };
 
+    int inputResult = userInput();  // 入力された値を取得
 
-    printf("実際のサイコロの出目: %d\n", diceRoll);
+    if (inputResult == evenFlag) {
+        printf("正解\n");
+    } else {
+        printf("不正解。%dで%s\n", dice, evenFlag ? "丁" : "半");
+    }
 
     return 0;
 }
